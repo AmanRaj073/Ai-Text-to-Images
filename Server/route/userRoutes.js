05ec5efcea4e4e67e5a38d981c002ab00b6d92b1
AmanRaj028 <aman.raj@neurapses.com>
2025-03-09 17:38:08 +0530

    minor changes

--- Server/route/userRoutes.js
+++ Server/route/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, userCredits } from "../controller/userController.js";
+import { registerUser, loginUser, userCredits, paymentRazorpay } from "../controller/userController.js";
 import userAuth from "../middleware/auth.js";
 
 const userRouter = express.Router();
@@ -7,5 +7,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/credits", userAuth, userCredits);
+userRouter.post("/payrazor", userAuth, paymentRazorpay);
 
 export default userRouter;
